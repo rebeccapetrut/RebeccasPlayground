5ec0496b6ca06fa4f46221ebcfcbc4e65ed9beed
petrutrebecca <rebecca.petrut@yahoo.com>
2021-10-22 20:25:59 +0300

    completed homework

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,34 @@
 public class Main {
     public static void main(String[] args) {
+       //1.
         System.out.println("Hola Rebecca!");
-        
+
+        //2.
+        System.out.println(2+2);
+
+        //3.
+        System.out.println(60 / 10);
+        //pentru impartire a unui numar care nu e intreg
+        float myFloat = 10f;
+        float myFloat2 = 3f;
+        System.out.println(myFloat / myFloat2);
+
+
+
+        //4 a.
+        System.out.println(-5 + (8 * 6));
+
+        //b.
+        System.out.println((55 + 9) % 9);
+
+        //c.
+        System.out.println(20 + (-3 * 5 / 8));
+
+        //d.
+        System.out.println( 5 + (15 / 3 * 2)- (8 % 3));
+
+
+
     }
 
 
