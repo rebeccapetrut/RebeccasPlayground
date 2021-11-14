9bc02b9efeda1ac67914f1bdc5ee40970d9936a8
petrutrebecca <rebecca.petrut@yahoo.com>
2021-11-14 20:02:26 +0200

    completed homework4

---
+++ src/Lab4.java
@@ -0,0 +1,37 @@
+public class Lab4 {
+
+    public static void main(String[] args) {
+        LogicalOperation op = new LogicalOperation();
+
+        op.showNumbers(4);
+        op.showNumbersReverse(5);
+        op.showTheTheNumbers(5,9);
+        op.biggerNumber(25,10);
+        op.evenNumber(1,100);
+        op.oddNumber(1,100);
+        System.out.println(op.sumTheNumbers(90));
+        System.out.println(op.sumAndAvarage(90));
+        op.printStars();
+        op.countNumbers(90);
+        op.countNumbersBackwords(90);
+        op.countBetweenNumbers(5,10);
+        op.verifyBiggerNumber(20,5);
+        op.evenNumbers(1,100);
+        op.oddNumbers(1,100);
+        op.sumTheInterval(111,8899);
+        op.getAverageDivisibleBySeven(1,100);
+        op.FibonacciPower();
+        op.CozaLozaWoza(1,100);
+
+
+
+
+
+
+
+
+    }
+
+
+
+}
--- src/LogicalOperation.java
+++ src/LogicalOperation.java
@@ -1,4 +1,4 @@
-public class LogicalOperation {
+public class LogicalOperation{
 
     //Tema ex. 4
 
@@ -46,9 +46,7 @@ public class LogicalOperation {
 
     //Tema ex. 8
     public void givenNumber (int first){
-        String x = "The number is ";
-        int y = 2;
-        switch (y){
+        switch (first){
             case 1:
                 System.out.println("The number is 1");
                 break;
@@ -104,4 +102,280 @@ public class LogicalOperation {
         }
 
 
+
+    //14.11.21
+    //Tema ex. 1
+
+    public void showNumbers (int first){
+        for (int x = first; x <= 100; x++){
+            System.out.println(x);
+        }
     }
+
+    //Tema ex. 2
+
+    public void showNumbersReverse (int first){
+        for(int x = first; x >= -100; x--){
+            System.out.println(x);
+        }
+    }
+
+    //Tema ex. 3
+
+    public void showTheTheNumbers(int first, int last){
+        for (int x = first; x <= last; x++){
+            System.out.println(x);
+        }
+    }
+
+//    //Tema ex. 4
+
+    public void biggerNumber(int first, int last){
+        for (int x = first; x <= last; x++) {
+            System.out.println(x);
+        }
+            for (int y = last; y <= first; y++){
+                System.out.println(y);
+            }
+    }
+
+
+
+    //Tema ex. 5
+
+    public void evenNumber (int small, int big) {
+        for (int x = small; x <= big; x++) {
+            if (x % 2 == 0) {
+                System.out.println(x);
+            }
+        }
+    }
+
+    //Tema ex. 6
+
+    public void oddNumber (int small, int big){
+        for (int x = small; x <= big; x++){
+            if (x % 2 == 1){
+                System.out.println(x);
+            }
+        }
+    }
+
+
+    //Tema ex. 7
+    public int sumTheNumbers(int first) {
+        int sum = 0;
+        for (int x = first; x <= 100; x++) {
+            sum = x + sum;
+        }
+        return sum;
+    }
+
+    //Tema ex. 8
+    public float sumAndAvarage (int first){
+        int sum = 0;
+        for (int x = first; x <= 100; x++){
+            sum = x + sum;
+        }
+        return (float) sum/ (100 - first + 1);
+    }
+
+   //Tema ex. 9
+    public void printStars(){
+    for (int x = 7; x >= 0; x--){
+        System.out.println("*");
+        System.out.println(" ");
+        for (int y = 1; y < x; y++) {
+            System.out.print("*");
+        }
+    }
+}
+
+
+    //Tema ex. 1 While
+    public void countNumbers (int first){
+        int x = first;
+        while (x <= 100){
+            System.out.println(x);
+            x++;
+        }
+    }
+
+    //Tema ex. 2
+    public void countNumbersBackwords (int first){
+        int x = first;
+        while (x >= -100){
+            System.out.println(x);
+            x--;
+        }
+    }
+
+    //Tema ex. 3
+    public void countBetweenNumbers (int first, int last){
+        int x = first;
+        while (x <= last){
+            System.out.println(x);
+            x++;
+        }
+    }
+
+    //Tema ex. 4
+    public void verifyBiggerNumber(int small, int big){
+        int x = small;
+        int y = big;
+        while (x <= y){
+            x++;
+            System.out.println(x);
+        }
+        while (y <= x){
+            y++;
+            System.out.println(y);
+        }
+  }
+
+
+    //Tema ex. 5
+    public void evenNumbers (int first, int last){
+        int x = first;
+        while (x <= last){
+            if (x % 2 == 0){
+            System.out.println(x);
+        }
+            x++;
+        }
+    }
+
+    //Tema ex. 6
+    public void oddNumbers (int first, int last){
+        int x = first;
+        while (x <= last){
+            if (x % 2 ==1){
+                System.out.println(x);
+            }
+            x++;
+        }
+    }
+
+    //Tema ex. 7
+
+    public void sumTheInterval (int first, int last){
+        int x = first;
+        float sum = 0;
+        int count = 0;
+        while ( x <= last){
+            sum = x + sum;
+            count++;
+            x++;
+        }
+        System.out.println(sum/count);
+    }
+
+
+    //Tema ex. 8
+    public void getAverageDivisibleBySeven(int first, int last){
+        int x = first;
+        float sum = 0;
+        int count = 0;
+        while (x <= last){
+            if (x % 7 == 0){
+                sum = x+ sum;
+                count++;
+            }
+            x++;
+        }
+        System.out.println(sum/count);
+    }
+
+//    //Tema ex. 9
+    public void FibonacciPower(){
+        int count = 20;
+        int x = 0;
+        int y = 1;
+        int z = 1;
+        while (z < count){
+            int sum = x + y;
+            x = y;
+            y = sum;
+            System.out.print(x + ", ");
+            z++;
+        }
+        System.out.println("");
+
+    }
+
+    //Tema ex. 10
+
+    // Nu stiu exact daca macar m-am apropiat cat de cat de idea corecta a codului, daaaar, exceptand faptul ca
+    // nu-mi mai dau seama cum ar trebui sa fac sa imi afiseze doar 11 pe linie, daca vreau sa le scriu while in while
+    // (si ma gandesc ca asa ar si trebui scrise) imi rezulta un sir de cifre care nu prea are vreun sens,
+    // iar daca le scriu fiecare while separat mi-l executa doar pe primul (cel cu multiplu de 3)
+    // si restul numerelor mi le afiseaza normal, deci ma gandesc ca ideea e oarecum buna dar imi scapa ceva :))
+    // anyway.. o sa mai lucrez in seara asta la el poate ii dau de capat :D
+
+    public void CozaLozaWoza(int first, int last) {
+        int a = first;
+        int x = last;
+        while (a < last) {
+            if (a % 3 == 0) {
+                System.out.print(" Coza ");
+            } else if (a % 3 != 0) {
+                System.out.print(a);
+            }
+            a++;
+        }
+
+        while (a < last) {
+            if (a % 5 == 0) {
+                System.out.print(" Loza ");
+            } else if (a % 5 != 0) {
+                System.out.print(a);
+            }
+            a++;
+        }
+
+        while (a < last) {
+            if (a % 7 == 0) {
+                System.out.print(" Woza ");
+            } else if (a % 7 != 0) {
+                System.out.print(a);
+            }
+            a++;
+        }
+
+            while (a < last) {
+                if (a % 3 == 0 && a % 5 == 0) {
+                    System.out.print(" CozaLoza ");
+                } else if (a % 3 != 0 && a % 5 != 0) {
+                    System.out.print(a);
+                }
+                a++;
+            }
+
+                while (a < last) {
+                    if (a % 3 == 0 && a % 7 == 0) {
+                        System.out.print(" CozaWoza ");
+                    } else if (a % 3 != 0 && a % 7 != 0) {
+                        System.out.print(a);
+                    }
+                    a++;
+                }
+
+                    while (a < last) {
+                        if (a % 5 == 0 && a % 7 == 0) {
+                            System.out.print(" WozaLoza ");
+                        } else if (a % 5 != 0 && a % 7 != 0) {
+                            System.out.print(a);
+                        }
+                        a++;
+                    }
+
+                        while (a < last) {
+                            if (a % 3 == 0 && a % 5 == 0 && a % 7 == 0) {
+                                System.out.print(" ColaLozaWoza ");
+                            } else if (a % 3 != 0 && a % 5 != 0 && a % 7 != 0) {
+                                System.out.print(a);
+                            }
+                            a++;
+                        }
+                    }
+                }
